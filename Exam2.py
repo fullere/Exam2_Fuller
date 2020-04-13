388a92114530833adcfd9b01c948fcd6f8f754e1
fullere <fullere@hartwick.edu>
2020-04-13 10:32:28 -0400

    finished code

--- Exam2.py
+++ Exam2.py
@@ -1,11 +1,18 @@
 # Elizabeth Fuller
 # Exam 2
+# 4/13/2020
 
-#import random
-newList = []
-cont = True
-while cont is True:
- # ask what method use wants
+# import statements
+import random
+from Search_Methods import bubble_sort
+from Search_Methods import selection_sort
+from Search_Methods import insertion_sort
+from Search_Methods import merge_sort
+from Search_Methods import quick_sort
+
+# using method to allow for looping
+def letsSort(list):
+ # ask what method user wants
     print("""How would you like to sort the list:
 Bubble Sort [1]
 Selection Sort [2]
@@ -19,15 +26,37 @@ Quit [6]
     # check choice for quit option
     if choice == 6:
         print("Quiting")
+        return
     # ask how many numbers in list
     print("How many random integers do you want in the list?")
     num = int(input("> "))
     # add random numbers to list
-    for num in newList:
+    x = 0
+    for x in range(num):
+        number = random.randint(1, 10000)
+        list.append(number)
+        x = x + 1
 
-
-    # send list to sort method
+    #send list to sort method
+    if choice == 1:
+        list = bubble_sort(list)
+    elif choice == 2:
+        list = selection_sort(list)
+    elif choice == 3:
+        list = insertion_sort(list)
+    elif choice == 4:
+        list = merge_sort(list)
+    elif choice == 5:
+        list = quick_sort(list)
     #print list
+    print(list)
+    print()
     #clear list
-    #loop back to start
+    list.clear()
 
+    #loop back to start
+    letsSort(list)
+
+# create list and call method
+newList = []
+letsSort(newList)
